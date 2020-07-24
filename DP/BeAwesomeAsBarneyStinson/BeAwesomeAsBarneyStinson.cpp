5b7b54cb62d6a6b5c8fb11d18f772250769564b6
daviddelapuente <daviddelapuentesalmon@gmail.com>
2020-07-23 20:26:32 -0400

    Barney stinson changed

--- DP/BeAwesomeAsBarneyStinson/BeAwesomeAsBarneyStinson.cpp
+++ DP/BeAwesomeAsBarneyStinson/BeAwesomeAsBarneyStinson.cpp
@@ -4,26 +4,27 @@
 //./name < name.txt
 //url: https://www.spoj.com/problems/BEHAPPY/
 
-std::vector < std::vector < int> > DP(100,std::vector<int> (100, 0));
 int main() {
     
     int M;
     int N;
     while(std::cin>>M && std::cin>>N){
+        if(M==0 && N==0){
+            break;
+        }
+        int numPaths=0;
+        std::vector < std::vector < int> > DP(M,std::vector<int> (100, 0));
         for(int i=0;i<M;i++){
             int a;
             int b;
             std::cin>>a>>b;
-            if(i==0){
-                for(int j=a;j<b;j++){
-                    if(a<=N && N<=b){
-                        DP[i][j]=1;
-                    }
+
+            if(){
+                for(int j=a;j<=b;j++){
+                    
                 }
             }
-            for(int j=a;j<b;j++){
-                
-            }
+            
         }
     }
     return 0;
