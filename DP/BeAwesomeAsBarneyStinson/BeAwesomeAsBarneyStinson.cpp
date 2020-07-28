b105cf3c108fc6c01bf00d20b1fc69d312bdb8d1
daviddelapuente <daviddelapuentesalmon@gmail.com>
2020-07-27 21:33:35 -0400

    be awsome as barney stinson solved

--- DP/BeAwesomeAsBarneyStinson/BeAwesomeAsBarneyStinson.cpp
+++ DP/BeAwesomeAsBarneyStinson/BeAwesomeAsBarneyStinson.cpp
@@ -13,19 +13,30 @@ int main() {
             break;
         }
         int numPaths=0;
-        std::vector < std::vector < int> > DP(M,std::vector<int> (100, 0));
+        std::vector < std::vector < int> > DP(M,std::vector<int> (N+1, 0));
         for(int i=0;i<M;i++){
             int a;
             int b;
             std::cin>>a>>b;
 
-            if(){
+            if(i==0){
                 for(int j=a;j<=b;j++){
-                    
+                    DP[i][j]=1;
                 }
+            }else{
+                for(int j=0;j<N;j++){
+                    for(int k=a;k<=b;k++){
+                        int tentativeN=k+j;
+                        if (tentativeN<=N){
+                            DP[i][tentativeN]+=DP[i-1][j];
+                        }
+                    }
+                }
+
             }
             
         }
+        std::cout<< DP[M-1][N] <<std::endl;
     }
     return 0;
 }
