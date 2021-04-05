2cef96fb9c8309863ad92e7bff51c6c4f117c880
daviddelapuente <you@example.com>
2021-04-05 02:03:27 -0400

    vitaly and pie solved

---
+++ hash/vitalyAndPie/vitalyAndPie.cpp
@@ -0,0 +1,72 @@
+#include <iostream>
+#include <map>
+//g++ name.cpp -o name
+//./name < test.txt
+// url: https://codeforces.com/problemset/problem/525/A
+
+std::map<long, long> H;
+
+long dollars(long n){
+    if(n<=3){
+        return H[n];
+    }else{
+        if( H.find(n) == H.end()){
+            H[n]=std::max(dollars(n/2)+dollars(n/3)+dollars(n/4),n);
+        }
+        return H[n];
+    }
+}
+
+int main() {
+    
+    int n;
+    std::cin>>n;
+    n--;
+    char rooms[2*n];
+    std::cin>>rooms;
+    
+    long ks=0;
+
+    for(int i=0;i<2*n;i=i+2){
+        
+        char key=rooms[i];
+        char lock=rooms[i+1]+32;
+        
+        if( H.find(key) == H.end()){
+            //if we havent see the key before
+            H[key]=1;
+        }else{
+            //we add the key to our hash
+            H[key]++;
+        }
+
+        
+
+        //now check the lock
+
+
+        if(H.find(lock) == H.end()){
+            //we havent seen the lock, we need to buy a key
+            H[lock]=0;
+            ks++;
+
+        }else{
+            
+            //if we have seen the lock in our hash
+
+            if(H[lock]>0){
+                //if we have keys to use
+                H[lock]--;
+            }else{
+                //if we dont have keys we buy one
+                ks++;
+            }
+        }
+
+    }
+
+    std::cout<<ks<<std::endl;
+
+
+    return 0;
+}
